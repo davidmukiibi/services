44bf9eda6fc6dd3fe2b9b497a19af833c9bf5b98
David Mukiibi <david.mukiibiq@gmail.com>
2021-03-06 13:10:28 +0300

    Update db.go

--- db.go
+++ db.go
@@ -7,6 +7,8 @@ import (
 
 	"github.com/jinzhu/gorm"
 	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 
 	// Postgres gorm driver
 	_ "github.com/jinzhu/gorm/dialects/postgres"
@@ -22,9 +24,12 @@ func DbConnect() (*gorm.DB, error) {
 	SSLMode := os.Getenv("SSL_MODE")
 	dbType := os.Getenv("DB_TYPE")
 
-	connectionString := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=%s password=%s", dbHost, dbUser, dbName, "disable", dbPass)
+        dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=%s port=5432", dbHost, dbUser, dbPass, dbName, SSLMode)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
-	db, err := gorm.Open(dbType, connectionString)
+	// connectionString := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=%s password=%s", dbHost, dbUser, dbName, "disable", dbPass)
+
+	// db, err := gorm.Open(dbType, connectionString)
 
 	if err != nil {
 		log.Fatalln("Error connecting to database", err)
